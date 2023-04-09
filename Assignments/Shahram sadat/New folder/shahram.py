7252b5c6d70caab54ed0d99fcfc95c08431ae2b5
Shahramsadat <127715740+Shahramsadat@users.noreply.github.com>
2023-04-09 20:50:06 +0430

    emoji.py

--- Assignments/Shahram sadat/New folder/shahram.py
+++ Assignments/Shahram sadat/New folder/shahram.py
@@ -1,4 +1,9 @@
-user_input = int(input("How many times do you want to print"))
+inp = input("do you want to stop")
+while inp != 'stop':
 
-for i in range(user_input):
-	print("Printing it now...")
+ 	input1 = int(input("while number multipliction do you want?"))
+ 	input2 = int(input("upto which number you want it to run?"))
+
+ 	for i in range(input2):
+ 		print(f"{i} X {input1} = {i * input1")
+ 	inp = input("which number multipaltion do you want?")
